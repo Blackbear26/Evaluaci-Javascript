77287f60ca5db227136eea4ebe7d155fb2ee961e
blackbear26 <163040073+Blackbear26@users.noreply.github.com>
2024-10-07 13:40:37 -0600

    aqui mi ejercicio

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let nombre = 'yatziel';
+let edad = 26;
+var estudiaste = true
+
+console.log(nombre);
+console.log(edad);
+console.log(estudiaste);
+
+alert("¡hola, soy " + nombre + " y tengo " + edad + " años ");
+
+if (estudiaste >= true) {alert(nombre + " si estudiaste! pasaras la materia! 😀 ");}
+else {alert(nombre + " no estudiaste! reprobaras la materia! 😪 ");}
+
+nombre = prompt("¿cual es tu nombre?");
+edad = prompt("¿cual es tu edad?")
+
+
+
+
